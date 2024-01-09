8140facd1455723dbb53901ea35f6b34971ffaa8
Sivakumar A <155511908+SivaA2810@users.noreply.github.com>
2024-01-09 16:35:06 +0530

    Add files via upload

--- Question_D.java
+++ Question_D.java
@@ -2,9 +2,28 @@ package javaTask_B;
 
 public class Question_D {
 
+//	Write a program to print the given below pattern.
+//	Sample Input"
+//	*   *
+//   * *
+//    *
+//   * *
+//  *   *
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 
-	}
-
+		   int size =5;
+		        for (int row = 1; row <= size; row++) {
+		            for (int colomn = 1; colomn <= size; colomn++) {
+		                if (row == colomn || row + colomn == size + 1) {
+		                    System.out.print("*");
+		                } else {
+		                    System.out.print(" ");
+		                }
+		            }
+		            System.out.println();
+		        }
+		    }
+			
 }
